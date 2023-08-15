2f4485fa6a43778cb9bec6842d5585eae467e290
UYUYANMARUL <ataberkcekic@hotmail.com.tr>
2023-08-15 19:50:29 +0300

    course

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,74 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScop = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScop.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We Have Already Data");
+            }
+        }
+    }
+}
